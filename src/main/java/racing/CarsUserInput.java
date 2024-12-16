2bc80d487c296a3229611215e9fc8a289f3d1846
chumin-yeong <chu35my@gmail.com>
2024-12-17 00:03:18 +0900

    자동차이름을 사용자에게 받는 클래스 ,구분하는 함수

---
+++ src/main/java/racing/CarsUserInput.java
@@ -0,0 +1,28 @@
+package racing;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class CarsUserInput {
+    private String carsNameInput;
+
+    public CarsUserInput(String carsNameInput) {
+        this.carsNameInput = carsNameInput;
+    }
+
+    //차 이름 입력받기
+    public static List<Car> getInputCar() {
+        //이 함수 안에는 List<String> 의 값이 있어 이거를 List<Car>로 바꿔줄거야
+        return getInputStringCar().stream()
+                .map((gameCar) -> new Car(gameCar)).toList();
+    }
+
+    private static List<String> getInputStringCar() {
+        System.out.println("레이스에 참가할 자동차들의 이름을 쉼표(,)로 구분하여 입력하세요.");
+        Scanner scanner = new Scanner(System.in);
+        String gameCars= scanner.nextLine();
+        return Arrays.asList(gameCars.split(","));
+    }
+
+}
