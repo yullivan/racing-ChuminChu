e02912950e0fad4d0663afdee7f4f7a66ef8152a
chumin-yeong <chu35my@gmail.com>
2024-12-16 23:54:07 +0900

    자동차 한대의 이름, 위치 생성 <예외처리> 이름 - 10자 넘어가면 안됨 <함수> 1. 랜덤값이 3이상이면 자동차의 위치 값이 +1이 되고 3미만이면 멈춰있는 positionPlus함수 2. 1-6까지 랜덤하게 발생하는 함수

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -1,18 +1,31 @@
 package racing;
 
-public class Car {
-    private String carName;
-    private int position = 0;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
 
-    public Car(String carName) {
-        if (carName.length() > 11){
-            throw new IllegalArgumentException("[ERROR] 차의 이름은 10자 이하로 적어주세요.");
+public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        if (name.length() > 10) {
+            throw new IllegalArgumentException("[ERROR] 자동차의 이름은 10글자를 넘어갈 수 없습니다.");
         }
-        this.carName = carName;
+        this.name = name;
     }
 
-    public Car(int position) {
+    public Car(String name, int position) {
+        if (name.length() > 10) {
+            throw new IllegalArgumentException("[ERROR] 자동차의 이름은 10글자를 넘어갈 수 없습니다.");
+        }
+        this.name = name;
         this.position = position;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getPosition() {
@@ -23,11 +36,40 @@ public class Car {
         this.position = position;
     }
 
-    public String getCarName() {
-        return carName;
+    //근데 또 이렇게 쓰면 차 3대에 대한 정보는 안주고 하나에 대한 것만 주니까 이 함수만 Cars로 옮기자
+//    public List<String> print() {
+//        String print = "-";
+//        List<String> carMove = new ArrayList<>();
+//
+//        carMove.add(this.getName() + " 🚗" + print.repeat(positionPlus(this)));
+//        return carMove;
+//    }
+
+    public static int positionPlus(Car car) {
+        if (generationRandomNum() >= 3) {
+            car.setPosition(car.getPosition() + 1);
+        }
+        return car.getPosition();
     }
 
-    public void setCarName(String carName) {
-        this.carName = carName;
+    // 1. 랜덤수 발생
+    private static int generationRandomNum() {
+        Random random = new Random();
+        int num = random.nextInt(1, 7);
+        return num;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return position == car.position && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
     }
 }
